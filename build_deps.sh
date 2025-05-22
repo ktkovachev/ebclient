3227bfc9ea07a1b5bd2ec91440b844a95ccf0432
Kiril Kovachev <ktkovachev@gmail.com>
2025-05-22 01:55:27 +0100

    refactor: use && operator for sequencing commands to avoid bad failure cases (along with set -e)

--- build_deps.sh
+++ build_deps.sh
@@ -2,14 +2,10 @@
 set -e
 
 cd lib/ebu
-./configure
-make
-sudo make install
+./configure && make && sudo make install
 
 cd ../libmxml
-./configure
-make
-sudo make install
+./configure && make && sudo make install
 
 which apt && sudo apt install lib32z1-dev || echo "Please install a static zlib (libz.a) onto your system and ensure it is available on the linker path." >&2
 
